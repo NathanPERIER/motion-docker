931ab83dccb425057986ad7c8248ed46ad5dbf14
NathanPERIER <periernathan2000@gmail.com>
2023-09-26 20:25:04 +0200

    Added test scripts

---
+++ scripts/discord/motion-detected.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import json
+
+from discord_webhook import DiscordWebhook
+
+
+def main() :
+
+    webhook_url = os.getenv('WEBHOOK_URL')
+
+    webhook = DiscordWebhook(url=webhook_url)
+    webhook.set_content(f"Motion detected\n`{json.dumps(sys.argv)}`")
+
+    webhook.execute()
+
+
+if __name__ == '__main__' :
+    main()
